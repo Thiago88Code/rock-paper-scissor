c2fe49d09a1ae50b7984638dd2d04afc2c57c5d8
ThiagoSz <thdev88@gmail.com>
2023-05-18 14:52:39 +0100

    removing unnecessary comments

---
+++ src/reducers/scoreReducerTypes.ts
@@ -0,0 +1,47 @@
+
+export enum OptionActionKind {
+    UPDATE_PLAYER_CHOICE = 'UPDATE_PLAYER_CHOICE',
+    UPDATE_COMPUTER_CHOICE = 'UPDATE_COMPUTER_CHOICE',
+    RUN_TIMER = 'RUN_TIMER',
+    PLAYER_WINS = 'PLAYER_WINS',
+    COMPUTER_WINS = 'COMPUTER_WINS',
+    DRAW = 'DRAW',
+}
+
+interface UpdatePlayerChoice {
+    type: OptionActionKind.UPDATE_PLAYER_CHOICE
+    payload: number
+}
+
+interface UpdateComputerChoice {
+    type: OptionActionKind.UPDATE_COMPUTER_CHOICE
+    payload: number
+}
+
+interface RunTimer {
+    type: OptionActionKind.RUN_TIMER
+    payload: boolean
+}
+
+interface PlayerWins {
+    type: OptionActionKind.PLAYER_WINS
+    payload: string
+}
+
+interface ComputerWins {
+    type: OptionActionKind.COMPUTER_WINS
+    payload: string
+}
+
+interface Draw {
+    type: OptionActionKind.DRAW
+    payload: string
+}
+
+export type ActionTypes =
+    UpdatePlayerChoice 
+    | UpdateComputerChoice 
+    | RunTimer 
+    | PlayerWins 
+    | ComputerWins 
+    | Draw 
